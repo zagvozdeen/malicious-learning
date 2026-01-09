e6ee73d8be3ad0bba8560384f14e125d640b9229
Denis Zagvozdin <zagvozdeen@gmail.com>
2026-01-09 22:35:23 +0500

    feat: install vue-router and tailwindcss, add biome json config, format files

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import './style.css'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import CardPage from './pages/CardPage.vue'
+import MainPage from './pages/MainPage.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{ path: '/', component: MainPage },
+		{ path: '/card', component: CardPage },
+	],
+})
+
+createApp(App).use(router).mount('#app')
---
+++ src/pages/CardPage.vue
@@ -0,0 +1,3 @@
+<template>
+Card page
+</template>
