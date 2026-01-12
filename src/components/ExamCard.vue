dd0ade53c5f5b4b54637e9cd4b2202777e23ad43
Denis Zagvozdin <zagvozdeen@gmail.com>
2026-01-13 01:23:34 +0500

    feat: new intermidiate result

--- src/components/ExamCard.vue
+++ src/components/ExamCard.vue
@@ -1,37 +1,32 @@
 <template>
   <div
-    class="h-96 w-full perspective-[1000px] cursor-pointer"
+    class="p-4 rounded-4xl bg-gray-500/20 text-xl font-bold text-center select-none cursor-pointer"
+    v-show="isFront"
     @click="onClickCard"
   >
-    <div
-      class="relative h-full w-full transition-all duration-500 transform-3d"
-      :class="{ 'rotate-y-180': isFront }"
-    >
-      <div class="absolute inset-0 flex items-center justify-center rounded-4xl bg-gray-500/20 hover:bg-gray-500/25 p-4 text-white backface-hidden">
-        <h1 class="text-xl font-bold text-center">
-          {{ front }}
-        </h1>
-      </div>
-
-      <div class="absolute inset-0 flex items-center justify-center rounded-4xl bg-green-500/20 hover:bg-green-500/25 p-4 text-white rotate-y-180 backface-hidden">
-        <span
-          class="text-base text-center"
-          v-html="back"
-        />
-      </div>
-    </div>
+    {{ front }}
   </div>
+  <div
+    class="p-4 rounded-4xl bg-green-500/20 text-sm text-left select-none cursor-pointer"
+    v-show="!isFront"
+    @click="onClickCard"
+    v-html="back"
+  />
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
-const isFront = ref(false)
+const isFront = ref(true)
 
 const { front, back } = defineProps<{
   front: string,
   back: string,
 }>()
+
+watch(() => front, () => {
+  isFront.value = true
+})
 
 const onClickCard = () => {
   isFront.value = !isFront.value
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,29 +1,43 @@
 <template>
   <div class="min-h-dvh w-full flex items-center justify-center">
-    <div class="w-full flex flex-col items-center gap-4">
-      <button
-        class="bg-green-700 hover:bg-green-800 transition uppercase font-bold text-sm w-full rounded p-2 cursor-pointer"
-        type="button"
-        @click="onAllQuestions"
-      >
-        Все вопросы подряд
-      </button>
-      <button
-        class="bg-green-700 hover:bg-green-800 transition uppercase font-bold text-sm w-full rounded p-2 cursor-pointer"
-        type="button"
-        @click="onAllQuestions"
-      >
-        Все вопросы вперемешку
-      </button>
-    </div>
+    <ul class="flex flex-col gap-px w-full rounded-2xl border border-gray-500/30 overflow-hidden">
+      <li class="w-full">
+        <button
+          class="grid grid-cols-[min-content_1fr_min-content] items-center w-full gap-2 p-2 cursor-pointer bg-gray-500/20 hover:bg-gray-500/30"
+          type="button"
+          @click="onAllQuestions"
+        >
+          <span class="size-6 flex items-center justify-center rounded-lg bg-blue-400">
+            <i class="bi bi-check-square-fill text-sm" />
+          </span>
+          <span class="text-left text-sm font-medium">Все вопросы подряд</span>
+          <span class="text-gray-400">
+            <i class="bi bi-chevron-right text-sm" />
+          </span>
+        </button>
+      </li>
+      <li class="w-full">
+        <button
+          class="grid grid-cols-[min-content_1fr_min-content] items-center w-full gap-2 p-2 cursor-pointer bg-gray-500/20 hover:bg-gray-500/30"
+          type="button"
+          @click="onAllQuestions"
+        >
+          <span class="size-6 flex items-center justify-center rounded-lg bg-red-400">
+            <i class="bi bi-shuffle text-sm" />
+          </span>
+          <span class="text-left text-sm font-medium">Все вопросы вперемешку</span>
+          <span class="text-gray-400">
+            <i class="bi bi-chevron-right text-sm" />
+          </span>
+        </button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
-import { ref } from 'vue'
 
-const test = ref(false)
 const router = useRouter()
 const onAllQuestions = () => {
   router.push({ name: 'card' })
